d75b10fddf99cac5d17275f2d61fd63f525b72f4
Eastodev Roy <163621268+eastodev288@users.noreply.github.com>
2024-11-11 19:28:17 +0600

    GestureDetector

--- main.dart
+++ main.dart
@@ -1,5 +1,4 @@
-// ignore_for_file: prefer_const_constructors
-
+// ignore: file_names
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,20 +7,26 @@ void main() {
 
 // ignore: must_be_immutable
 class MyApp extends StatelessWidget {
+  // ignore: prefer_const_constructors_in_immutables
   MyApp({super.key});
+  void userTaped() {
+    print("hello");
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: GridView.builder(
-          itemCount: 64,
-          gridDelegate:
-              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
-          itemBuilder: (context, index) => Container(
-            color: Colors.deepOrange,
-            margin: EdgeInsets.all(2),
+        body: Center(
+          child: GestureDetector(
+            onTap: (userTaped),
+            child: Container(
+              height: 200,
+              width: 200,
+              color: Colors.deepPurple[200],
+              child: Center(child: Text("Click meee!!")),
+            ),
           ),
         ),
       ),
