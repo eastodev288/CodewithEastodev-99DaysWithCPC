f3208c9b0938528a8cac07feb34941f63e560ffd
Eastodev Roy <163621268+eastodev288@users.noreply.github.com>
2024-11-23 23:52:12 +0600

    Tab Bar
    day 23 . I learn Tab bar .

--- home_page.dart
+++ home_page.dart
@@ -1,59 +1,66 @@
 import 'package:flutter/material.dart';
 
-import '../util/dialog_box.dart';
-import '../util/todo_tile.dart';
-
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  //List of todo task
-  List toDoList = [
-    ["Make Tutorial", false],
-    ["Do Exercxise ", false],
-  ];
-  //checkbox was tapped
-  void checkBoxChanged(bool? value, int index) {
-    setState(() {
-      toDoList[index][1] = !toDoList[index][1];
-    });
-  }
-
-  //creat a new task
-  void createNewTask() {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return DialogBox();
-      },
-    );
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.yellow[200],
-      appBar: AppBar(
-        title: const Text('                             To Do'),
-        elevation: 0,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
-      ),
-      body: ListView.builder(
-        itemCount: toDoList.length,
-        itemBuilder: (context, index) {
-          return ToDoTile(
-            taskName: toDoList[index][0],
-            taskCompleted: toDoList[index][1],
-            onChanged: (value) => checkBoxChanged(value, index),
-          );
-        },
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('T A B B A R'),
+          backgroundColor: Colors.brown,
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            const TabBar(
+              tabs: [
+                Tab(
+                  icon: Icon(
+                    Icons.home,
+                    color: Colors.deepPurple,
+                  ),
+                ),
+                Tab(
+                  icon: Icon(
+                    Icons.settings,
+                    color: Colors.deepPurple,
+                  ),
+                ),
+                Tab(
+                  icon: Icon(
+                    Icons.person,
+                    color: Colors.deepPurple,
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: TabBarView(children: [
+                Container(
+                  color: Colors.cyan,
+                  child: const Center(
+                    child: Text('1st Tab'),
+                  ),
+                ),
+                Container(
+                  color: const Color.fromARGB(255, 212, 162, 0),
+                  child: const Center(
+                    child: Text('2nd Tab'),
+                  ),
+                ),
+                Container(
+                  color: Colors.indigo,
+                  child: const Center(
+                    child: Text('3rd Tab'),
+                  ),
+                ),
+              ]),
+            )
+          ],
+        ),
       ),
     );
   }
