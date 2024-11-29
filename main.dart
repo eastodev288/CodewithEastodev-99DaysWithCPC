52aafc4541666ad1cef602219fc47f26f7dbd93f
Eastodev Roy <163621268+eastodev288@users.noreply.github.com>
2024-11-29 22:06:10 +0600

    how to add images..

--- main.dart
+++ main.dart
@@ -1,19 +1,34 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
-import 'home_page.dart';
-
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: Scaffold(
+        body: Container(
+          color: Colors.blueGrey,
+          //add imgs
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Card(
+                shadowColor: Colors.red,
+                elevation: 20.0,
+                child: Image.network(
+                  "https://imgs.search.brave.com/xRzvZPCGvqWHY_6Pnm_C1u2QqAEFlPJKC9TAoGsGXT0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTg1/MzA0Mjc0L3Bob3Rv/L2tpdHRlbi5qcGc_/cz02MTJ4NjEyJnc9/MCZrPTIwJmM9WFpZ/NVVsUEdrTEhCSGst/cFlUMGQzNm81NGdO/VW0tWW5RcWYtLTNi/MkU5QT0",
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
