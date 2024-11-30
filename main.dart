e487a5ca1288305348ea6afbaddaf81c33ac6a85
Eastodev Roy <163621268+eastodev288@users.noreply.github.com>
2024-11-30 23:54:30 +0600

    learn AppBar & Floating Action Button.

--- main.dart
+++ main.dart
@@ -1,31 +1,30 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Material App',
       home: Scaffold(
-        body: Container(
-          color: Colors.blueGrey,
-          //add imgs
-          child: Center(
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Card(
-                shadowColor: Colors.red,
-                elevation: 20.0,
-                child: Image.network(
-                  "https://imgs.search.brave.com/xRzvZPCGvqWHY_6Pnm_C1u2QqAEFlPJKC9TAoGsGXT0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTg1/MzA0Mjc0L3Bob3Rv/L2tpdHRlbi5qcGc_/cz02MTJ4NjEyJnc9/MCZrPTIwJmM9WFpZ/NVVsUEdrTEhCSGst/cFlUMGQzNm81NGdO/VW0tWW5RcWYtLTNi/MkU5QT0",
-                ),
-              ),
-            ),
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text("DIU"),
+          leading: Icon(Icons.menu_book),
+          actions: [
+            Icon(Icons.access_alarm),
+            Icon(Icons.notification_add_outlined),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text("DATA"),
+        ),
+        body: Center(
+          child: Text(
+            'We come DIU ',
           ),
         ),
       ),
