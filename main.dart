f442925fd7d74eb7b4a9b66c4fb563d56678467f
Eastodev Roy <163621268+eastodev288@users.noreply.github.com>
2024-12-11 23:42:36 +0600

    Contact List
    It's Day-41. I create a Contact List using List Tile.

--- main.dart
+++ main.dart
@@ -1,47 +1,159 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Material App Bar'),
+          title: const Text('Contact List'),
         ),
         body: ListView(
-          children: [
-            ListItem(color: Colors.blueGrey),
-            ListItem(color: Colors.cyan),
-            ListItem(color: Colors.indigo),
-            ListItem(color: Colors.cyanAccent),
-            ListItem(color: Colors.red),
-            ListItem(color: Colors.purple),
-            ListItem(color: Colors.pink),
-            ListItem(color: Colors.green),
+          children: const [
+            ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Color.fromARGB(255, 135, 243, 33),
+                child: Text("SM"),
+              ),
+              title: Text("Sujon Mia"),
+              trailing: Icon(Icons.call),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Color.fromARGB(255, 243, 145, 33),
+                child: Text("ERU"),
+              ),
+              title: Text("Eastodev Roy Utso"),
+              trailing: Icon(Icons.call),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Color.fromARGB(255, 33, 243, 96),
+                child: Text("YS"),
+              ),
+              title: Text("Yasin Shuvo "),
+              trailing: Icon(Icons.call),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Color.fromARGB(255, 243, 33, 79),
+                child: Text("JI"),
+              ),
+              title: Text(" Jahadul Islam Joy"),
+              trailing: Icon(Icons.call),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Color.fromARGB(255, 33, 243, 103),
+                child: Text("PR"),
+              ),
+              title: Text("Pakhi Roy"),
+              trailing: Icon(Icons.call),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Color.fromARGB(255, 243, 33, 96),
+                child: Text("OM"),
+              ),
+              title: Text("onu Mia"),
+              trailing: Icon(Icons.call),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.blue,
+                child: Text("NI"),
+              ),
+              title: Text("Nice Islam"),
+              trailing: Icon(Icons.call),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.grey,
+                child: Text("JM"),
+              ),
+              title: Text("jonhan Mia"),
+              trailing: Icon(Icons.call),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.red,
+                child: Text("IM"),
+              ),
+              title: Text("Ikbaal Mia"),
+              trailing: Icon(Icons.call),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.green,
+                child: Text("M"),
+              ),
+              title: Text("Mamun"),
+              trailing: Icon(Icons.call),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.yellow,
+                child: Text("S"),
+              ),
+              title: Text("Sunny "),
+              trailing: Icon(Icons.call),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.grey,
+                child: Text("JM"),
+              ),
+              title: Text("jonhan Mia"),
+              trailing: Icon(Icons.call),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.red,
+                child: Text("IM"),
+              ),
+              title: Text("Ikbaal Mia"),
+              trailing: Icon(Icons.call),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.grey,
+                child: Text("JM"),
+              ),
+              title: Text("jonhan Mia"),
+              trailing: Icon(Icons.call),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.red,
+                child: Text("IM"),
+              ),
+              title: Text("Ikbaal Mia"),
+              trailing: Icon(Icons.call),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.grey,
+                child: Text("JM"),
+              ),
+              title: Text("jonhan Mia"),
+              trailing: Icon(Icons.call),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.red,
+                child: Text("IM"),
+              ),
+              title: Text("Ikbaal Mia"),
+              trailing: Icon(Icons.call),
+            ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class ListItem extends StatelessWidget {
-  Color? bgColor;
-  ListItem({Key? key, color}) : super(key: key) {
-    bgColor = color;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(8.0),
-      child: Container(
-        height: 120.0,
-        width: double.infinity,
-        color: bgColor,
       ),
     );
   }
