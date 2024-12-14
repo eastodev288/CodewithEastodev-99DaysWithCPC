3051215ff3a3e59a1cb96184fa5041dbebbc2e5a
Eastodev Roy <163621268+eastodev288@users.noreply.github.com>
2024-12-14 22:19:52 +0600

    Use List View Builder using Map Data

--- main.dart
+++ main.dart
@@ -1,30 +1,27 @@
+import 'package:day_44/dataSource/users.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  Widget _ListItemGenersator(BuildContext contex, int i) {
-    return ListTile(
-      leading: CircleAvatar(
-        backgroundColor: Colors.blue,
-        child: Text(i.toString()),
-      ),
-      title: Text("Eastodev"),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text(' List View Builder '),
+          title: Text('Material App Bar'),
         ),
         body: ListView.builder(
-          itemCount: 100,
-          itemBuilder: (context, index) => _ListItemGenersator(context, index),
+          itemCount: Users.data.length,
+          itemBuilder: (BuildContext context, int i) => ListTile(
+            leading: CircleAvatar(
+              child: Text(Users.data[i]["id"].toString()),
+            ),
+            title: Text(Users.data[i]["name"]),
+            subtitle: Text(Users.data[i]["username"]),
+            trailing: Icon(Icons.person_2_outlined),
+          ),
         ),
       ),
     );
---
+++ users.dart
@@ -0,0 +1,44 @@
+class Users {
+  static List<Map> data = [
+    {
+      "id": 1,
+      "name": "W Ray",
+      "username": "Wablu",
+    },
+    {
+      "id": 2,
+      "name": "brome",
+      "username": "bro",
+    },
+    {
+      "id": 3,
+      "name": "ikbal islam",
+      "username": "ikbal",
+    },
+    {
+      "id": 4,
+      "name": "abir ahamed",
+      "username": "abir",
+    },
+    {
+      "id": 5,
+      "name": "Pakhi ray",
+      "username": "pakhi",
+    },
+    {
+      "id": 6,
+      "name": "Dolon roy",
+      "username": "dolon",
+    },
+    {
+      "id": 7,
+      "name": "Ripon Roy",
+      "username": "ripon",
+    },
+    {
+      "id": 8,
+      "name": "akash ray",
+      "username": "akash",
+    }
+  ];
+}
