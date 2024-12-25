313a588612945b49ec5c704d9ee2e917dc8cceca
Eastodev Roy <163621268+eastodev288@users.noreply.github.com>
2024-12-25 23:48:58 +0600

    Tab Battle game
    Its day 55.

--- main.dart
+++ main.dart
@@ -1,6 +1,77 @@
-import 'user.dart';
+import 'package:flutter/material.dart';
 
-void main() {
-  var user1 = User("eastodev", "232aed12", "eastodev22@gmail.com");
-  user1.login();
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int redScore = 50;
+  int blueScore = 50;
+  void redScoreUpadte() {
+    setState(() {
+      redScore = redScore + 1;
+      blueScore = blueScore - 1;
+    });
+  }
+
+  void blueScoreUpdate() {
+    setState(() {
+      blueScore = blueScore + 1;
+      redScore = redScore - 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Stack(
+          children: [
+            Column(
+              children: [
+                Expanded(
+                  flex: redScore,
+                  child: InkWell(
+                    onTap: redScoreUpadte,
+                    child: Container(
+                      color: Colors.redAccent,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: blueScore,
+                  child: InkWell(
+                    onTap: blueScoreUpdate,
+                    child: Container(
+                      color: Colors.blueAccent,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            redScore >= 100 || blueScore > 100
+                ? Container(
+                    width: double.infinity,
+                    height: double.infinity,
+                    color: Colors.purple.withOpacity(0.5),
+                    child: Center(
+                      child: Text(
+                        "${redScore >= 100 ? "Red" : "Blue"} Won!!",
+                        style: TextStyle(fontSize: 40.0, color: Colors.white),
+                      ),
+                    ),
+                  )
+                : Container(),
+          ],
+        ),
+      ),
+    );
+  }
 }
